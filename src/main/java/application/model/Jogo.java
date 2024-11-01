54d1ebd2a1d971f454f927a411cd15868b35f5f6
marcoweb <marcocarvalho.web@gmail.com>
2024-11-01 12:11:56 +0000

    Aula 01/11/2024

--- src/main/java/application/model/Jogo.java
+++ src/main/java/application/model/Jogo.java
@@ -2,6 +2,7 @@ package application.model;
 
 import java.util.Set;
 
+import application.record.JogoDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,9 +12,15 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "jogos")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Jogo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +33,10 @@ public class Jogo {
         joinColumns = @JoinColumn(name = "id_jogo"),
         inverseJoinColumns = @JoinColumn(name = "id_plataforma"))
     private Set<Plataforma> plataformas;
+
+    public Jogo(JogoDTO jogo) {
+        this.id = jogo.id();
+        this.titulo = jogo.titulo();
+        this.plataformas = jogo.plataformas();
+    }
 }
