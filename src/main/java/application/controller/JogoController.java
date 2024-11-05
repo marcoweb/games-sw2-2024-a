757bbaa7294f9b9ee80ec691823c9a0c8f7f9753
marcoweb <marcocarvalho.web@gmail.com>
2024-11-05 13:33:50 +0000

    Aula 05/11/2024

--- src/main/java/application/controller/JogoController.java
+++ src/main/java/application/controller/JogoController.java
@@ -1,8 +1,11 @@
 package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,15 @@ public class JogoController {
     @PostMapping
     public JogoDTO insert(@RequestBody JogoDTO jogo) {
         return jogoSrv.add(jogo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        jogoSrv.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public JogoDTO update(@PathVariable long id, @RequestBody JogoDTO jogo) {
+        return jogoSrv.update(id, jogo);
     }
 }
